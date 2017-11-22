c55d36d9e5f84036cdb6eabe3fb176c0942473c9
Carlos Camacho <ccamachog@avathartech.com>
2017-11-22 02:10:25 -0400

    Proyecto de correo transaccionales.

---
+++ src/main/java/edu/pucmm/mt/Main.java
@@ -0,0 +1,27 @@
+package edu.pucmm.mt;
+
+import com.sparkpost.Client;
+import com.sparkpost.exception.SparkPostException;
+
+public class Main {
+
+    /**
+     * Ejemplo de uso de correo transaccionales
+     * https://github.com/SparkPost/java-sparkpost
+     * @param args
+     * @throws SparkPostException
+     */
+    public static void main(String[] args) throws SparkPostException {
+
+        String API_KEY = "";
+        Client client = new Client(API_KEY);
+
+        client.sendMessage(
+                "noresponder@dominio",
+                "enviara@asd.com",
+                "Probando",
+                "Hola Mundo desde SparkPost",
+                "<b>Hola Mundo desde SparkPost</b>");
+
+    }
+}
